e3be663b745e9796ace8808fb049696b6cb0b811
Kam Kennedy <kameronk013@gmail.com>
2023-09-18 10:31:46 -0600

    add vendor names method

--- lib/market.rb
+++ lib/market.rb
@@ -5,4 +5,16 @@ class Market
     @name = market_name #I know convention may be to have these match but there are too many "name" variables floating in the different classes. 
     @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    vendor_names = []
+    @vendors.each do |vendor|
+      vendor_names << vendor.name
+    end
+    vendor_names
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -3,13 +3,15 @@ require "./spec/spec_helper"
 RSpec.describe "Market" do
   before(:each) do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    @item2 = Item.new({name: 'Tomato', price: '$0.50'}) 
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
-    @vendor2 = Vendor.new("Ba-Nom-a-Nom") 
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @market = Market.new("South Pearl Street Farmers Market")
   end
+
   describe "initialize" do
     it "exists" do 
       expect(@market).to be_an_instance_of(Market)
@@ -21,6 +23,32 @@ RSpec.describe "Market" do
 
     it "begins with an empty vendors array" do
       expect(@market.vendors).to eq([])
+    end
+  end
+
+  describe "#add vendor" do
+    it "adds instances of vendor to the vendors hash" do
+      @market.add_vendor(@vendor1)
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      expect(@market.vendors.length).to eq 1
+      @market.add_vendor(@vendor2)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      expect(@market.vendors.length).to eq 2
+      @market.add_vendor(@vendor3)
+      @vendor3.stock(@item1, 65)
+require 'pry'; binding.pry
+    end
+  end
+
+  describe "#vendor_names" do
+    it "returns an array of the vendor name strings" do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      @market.vendor_names
+    # expect(@market.vendor_names).to eq["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
   end
 end
