8a9125e6571553e18555ae31d348215815cd95fd
Kam Kennedy <kameronk013@gmail.com>
2023-09-18 10:42:07 -0600

    start vendors that sell method

--- lib/market.rb
+++ lib/market.rb
@@ -11,10 +11,14 @@ class Market
   end
 
   def vendor_names
-    vendor_names = []
-    @vendors.each do |vendor|
+    vendor_names = @vendors.map do |vendor|
       vendor_names << vendor.name
     end
-    vendor_names
+  end
+
+  def vendors_that_sell(item)
+    vendors_that_sell = @vendors.select do |vendor|
+      vendor.inventory.include?(item)
+    end
   end
 end
